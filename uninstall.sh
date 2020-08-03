be7bf83b25d756d0999a53876002f06777ab6fb0
royal <deepakchauhan878@gmail.com>
2020-08-03 04:54:20 +0000

    Changed uninstallation methods

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 sudo rm -rf /overheat-notifier
+sudo systemctl stop overheat-notifier.service
+sudo systemctl disable overheat-notifier.service
 sudo rm /etc/systemd/system/overheat-notifier.service
 echo "Overheat-Notifier removed successfully. Thank You for trying."

c6faab9f019d6af187c7f2f5d9b62ffba510966f
royal <deepakchauhan878@gmail.com>
2020-08-03 04:50:58 +0000

    Changed installation methods with single escript

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
+echo "Downloading Repo"
+git clone https://github.com/RoyalEagle73/Overheat-Notifier.git
+echo "Repository Downloaded"
+cd Overheat-Notifier
 echo "Checking/Installing dependencies"
 sudo pip3 install -r requirements.txt
 echo "Dependencies Installed Succesfully."
 echo "Copying current folder into root directory."
-echo "NOTE: Do not delete the folder afterwards." 
+echo "NOTE: Do not delete the folder afterwards."
 cp -r $PWD /
 echo "Folder Copied Successfully."
 echo "Making program to boot at startup."
